2bc78653207878f7deb3c5b78d1cb01640401967
clarkbar88 <tclark@herrerainc.com>
2022-11-15 11:34:13 -0800

    updates to performance, user interface

--- server.R
+++ server.R
@@ -4,61 +4,101 @@ library(shiny)
 library(dplyr)
 library(boot)
 library(ggplot2)
+library(rhandsontable)
 
-boot.mean<-function(x,i) mean(x[i])
+sigfigs <- function(x){
+  orig_scipen <- getOption("scipen")
+  options(scipen = 999)
+  on.exit(options(scipen = orig_scipen))
+  
+  x <- as.character(x)
+  x <- sub("\\.", "", x)
+  x <- gsub("(^0+|0+$)", "", x)
+  nchar(x)
+}
 
-
+initial_table<-read.table(
+text='"Effluent Concentration"	"Removal Efficiency (%)"
+5	72%
+5	83%
+5	92%
+7	76%
+5	63%
+10	71%
+5	51%
+4	58%
+8.04	64%
+5	76%
+5	76%
+14	60%
+5	73%
+NA	42%',header=T) %>%
+  rename(`Effluent Concentration`=1,`Removal Efficiency (%)`=2)
 
 server<-function(input,output){
-#   df<-reactive({
-#     read.table(text='5
-# 5
-# 5
-# 7
-# 5
-# 10
-# 5
-# 4
-# 8.04
-# 5
-# 5
-# 14
-# 5',col.names = c('values')) %>%
-#       mutate(values=as.numeric(values))
-#   })
-  df<-eventReactive(input$runIt,{
-  #  df<-as.data.frame(paste(input$pasted1, collapse = "\n"))
-    if(input$pasted1!=''){
-    read.table(text=input$pasted1,col.names = c('values')) %>%
-        mutate(values=as.numeric(values))
-    }
+
+df <- reactive({
+      hot <- input$hot
+      if (!is.null(hot)) hot_to_r(hot)
+    })
+
+output$hot <- renderRHandsontable({
+  rhandsontable(initial_table)
 })
 
+sigfig_num<-reactive({
+  ifelse(input$anal=='conc',
+         max(sigfigs(df()[!is.na(df()[,1]),1]),na.rm=T),
+         max(sigfigs(df()[!is.na(df()[,2]),2]),na.rm=T))
+})
 
-  bootOut<-reactive({
+  bootOut<-eventReactive(input$runIt,{
     if(nrow(df())>1){
-     df() %>%
+     df()  %>%
+        select(ifelse(input$anal=='conc',1,2)) %>%
+        rename(obs=1) %>%
+        filter(!is.na(obs)) %>%
+        mutate(obs=as.numeric(gsub('%','',obs))) %>%
         rsample::bootstraps(times=5000) %>%
         pull(splits) %>%
         purrr::map_dbl(.,function(x){ dat <-rsample::analysis(x)
-        mean(dat$values)
+        mean(dat$obs,na.rm=T)
         }) %>%
-        quantile(.95)
+        quantile(ifelse(input$anal=='conc',.95,0.05)) %>%
+        signif(.,sigfig_num())
     }
   })
   
-  output$plotOut<-renderPlot({
+
+  p<-eventReactive(input$runIt,{
     if(nrow(df())>1){
-    ggplot()+
-      geom_jitter(aes(x='',y=df()$values),height=0)+
-      geom_hline(yintercept=bootOut(),col='black',lwd=2)+
-     # geom_hline(yintercept=input$threshold,col='red',lwd=2)+
-      theme_bw()+
-      ylab('Values')+
-      xlab('')
+      df()%>%
+        rename(values=1,pctreduction=2) %>%
+        rowwise() %>%
+        mutate(pctreduction=as.numeric(gsub('%','',pctreduction)),
+               plotValue=ifelse(input$anal=='conc',values,pctreduction)) %>%
+        ggplot()+
+        geom_jitter(aes(x='',y=plotValue),height=0)+
+        geom_hline(yintercept=bootOut(),col='black',lwd=2)+
+        # geom_hline(yintercept=input$threshold,col='red',lwd=2)+
+        theme_bw()+
+        ylab(ifelse(input$anal=='conc','Effluent Concentrations','Removal Efficiency (%)'))+
+        xlab('')
     } else ggplot()
-})
+  }
+  )
+
+    output$plotOut<-renderPlot({p()})%>%
+      bindEvent(input$runIt)
+
   output$textOut<-renderText({
-    paste(input$ci,'% UCL:',bootOut())
-    })
+    paste('<center><p style="font-size:30px"><b>',
+          ifelse(input$anal=='conc','Upper 95% Confidence Limit for Effluent Concentrations:',
+                        'Lower 95% Confidence Limit for Removal Efficiency:'),
+          '<br>',
+          formatC(bootOut(),sigfig_num(),format='fg',flag='#'),
+          ifelse(input$anal=='conc','','%'),
+          '</b></p></center>') 
+    })%>%
+    bindEvent(input$runIt) 
 }
--- ui.R
+++ ui.R
@@ -1,15 +1,39 @@
 #User Interface
 
 library(shiny)
+library(rhandsontable)
 
-fluidPage(
+
+###requests - make it less crunchy/squishy (roll over on tables)
+##adjust y axis title based on which analysis
+
+ui<-fluidPage(fluidRow(column(8,titlePanel('Bootstrap Calculator')),
+              column(4,img(src='logo_horizontal.jpg',align='right',width='100%'))),
   sidebarLayout(
-  sidebarPanel(
-  textAreaInput("pasted1", "paste text here",height='200px'), 
-  radioButtons('ci','Select Confidence Interval',c('95%'=.95)),
+  sidebarPanel(radioButtons('anal','Select Analysis',c('Upper 95% Confidence Limit for Effluent Concentrations'='conc',
+                                        'Lower 95% Confidence Limit for Removal Efficiency'='removal')),
   numericInput('reps','Replications',5000),
-  actionButton('runIt','Calculate')),
+  actionButton('runIt','Calculate'),
+  width=3),
   mainPanel(
-  textOutput("textOut"),
-  plotOutput('plotOut')
-)))
+    HTML(paste0('This app can calculate either the one-tailed upper 95% confidence interval around the',
+          ' mean effluent concentration, or the one-tailed lower 95% confidence interval around the',
+          ' mean pollutant removal efficiency.  To perform these calcualtions, the app randomly ',
+          'resamples the original data to create 5000 datasets with the same number of values as the original data.',
+          'The mean of each resampled dataset is then calculated.',  
+          'The 5000 means are then sorted in ascending order.',
+          'The one-tailed upper 95% confidence interval around the mean effluent concentration is the',
+          ' mean with the rank of 4750 out of 5000.  The one-tailed lower 95% confidence interval around the ',
+          'mean pollutant removal efficiency is the mean with the rank of 250 out of 5000.  ',
+          '<br>',
+          'THIS APP SHOULD ONLY BE USED WHEN THERE ARE 10 OR MORE DATA POINTS FOR EFFLUENT CONCENTRATION OR ',
+          'POLLUTANT REMOVAL EFFICIENCY.')),
+    HTML('<br>'),
+    strong('Input Data'),
+    fluidRow(column(8,rHandsontableOutput('hot')),
+             column(4,plotOutput('plotOut'))
+             ),
+    htmlOutput("textOut"),
+    width=9)
+  )
+  )
